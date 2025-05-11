6c678bc704913699d5ae8229b744e0ca454e793f
Sagar <sagarawasthi.hiet@gmail.com>
2025-05-12 02:44:49 +0530

    First commit of lottery game

---
+++ lottery.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This application is a Lottery game created for fun.
+# Created by Sagar Awasthi
+
+echo Please enter a number between 0 to 10
+read userInput
+num=$(($RANDOM%11))
+if [ $userInput -eq $num ] ;
+then
+	echo You won the Lottery!
+else
+	echo You lose! The Lottery number was $num
+fi
